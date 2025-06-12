47bcf3944e89723399db2be0bdd70cd3afee11dd
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2025-06-13 00:28:55 +0300

    Update Brotli.Build.cs

--- Source/Brotli/Brotli.Build.cs
+++ Source/Brotli/Brotli.Build.cs
@@ -7,7 +7,7 @@ namespace UnrealBuildTool.Rules
         public Brotli(ReadOnlyTargetRules Target) : base(Target)
         {
             Type = ModuleType.External;
-            UndefinedIdentifierWarningLevel = WarningLevel.Off;
+            CppCompileWarningSettings.UndefinedIdentifierWarningLevel = WarningLevel.Off;
             bEnableExceptions = true;
             bUseRTTI = true;
 
